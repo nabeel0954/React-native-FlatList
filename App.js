afe82230051abf6a9330953d994cd7d19d3d6a0e
nabel ahmad <nabeel.ahmad7585@gmail.com>
2019-11-07 18:50:52 +0500

    App.js

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Image,FlatList,TouchableOpacity, Text, BackgroundImage } from 'react-native';
 
 const items = [
-  { thumbnail: { uri: 'https://cdn.pixabay.com/photo/2013/10/02/23/03/dawn-190055_960_720.jpg' } },
-  { thumbnail: { uri: 'https://cdn.pixabay.com/photo/2015/02/02/11/09/office-620822_960_720.jpg' } },
   { thumbnail: { uri: 'https://cdn.pixabay.com/photo/2015/06/24/15/45/ipad-820272_960_720.jpg' } },
   { thumbnail: { uri: 'https://cdn.pixabay.com/photo/2018/03/01/09/33/laptop-3190194_960_720.jpg' } },
   { thumbnail: { uri: 'https://cdn.pixabay.com/photo/2018/03/01/09/33/laptop-3190194_960_720.jpg' } },
